7921bb66057cb58c070462132d5efdfd3c738fba
Robin Alderin <94robald@gmail.com>
2021-01-30 21:43:32 +0100

    Fill Out
    Filled out the level and added the last object at a random location

---
+++ Assets/Robin/Scripts/Last_Pickup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Last_Pickup : MonoBehaviour
+{
+    public Transform[] lastPickUpLocations = new Transform[4];
+
+    public GameObject lastPickup;
+    void Start()
+    {
+        int variable = Random.Range(0, lastPickUpLocations.Length);
+        Instantiate(lastPickup, lastPickUpLocations[variable-1].position, lastPickUpLocations[variable-1].rotation);
+    }
+    void Update()
+    {
+        Debug.Log(lastPickup.transform.position);
+    }
+}
